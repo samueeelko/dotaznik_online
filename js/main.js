fb17e702b8c523c47f717ab22ce5ae020184c825
tankossamuel <samueltankos4@gmail.com>
2026-02-05 00:31:19 +0100

    opraveny supabse v scope

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const supabaseProject = 'https://fhoxdanqrvlmwwrukpux.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZob3hkYW5xcnZsbXd3cnVrcHV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0MTI4MzksImV4cCI6MjA4Mzk4ODgzOX0.fvF441pHhhvHEZkM7_-YHbEV4s9h3tjfojkWOPe0WWA'
-const supabase = window.supabase.createClient(supabaseProject, supabaseKey)
+const supabaseClient = window.supabase.createClient(supabaseProject, supabaseKey)
 
 
-initAuth(supabase)
+initAuth(supabaseClient)
 
 setupAuthListener()
 
